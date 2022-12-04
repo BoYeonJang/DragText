9650c4c819dd5d301172e9da35e50908444aef59
BoYeonJang <uko010199@gmail.com>
2022-12-05 01:13:23 +0900

    Initial Commit

---
+++ DragText/DragTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragTextApp.swift
+//  DragText
+//
+//  Created by Bo Yeon Jang on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct DragTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
